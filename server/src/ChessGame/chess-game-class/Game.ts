a5ef70563894a10cbf63315d2e70329bd759a950
Turbobot <turbobot@vercel.com>
2025-09-04 20:26:13 +0530

    feat(create-turbo): apply official-starter transform

--- server/src/ChessGame/chess-game-class/Game.ts
+++ server/src/ChessGame/chess-game-class/Game.ts
@@ -1,4 +1,4 @@
-import { Color, GameState, GameStatusEnum, Move, Position } from "../../types/types";
+import { Color, GameState, GameStatusEnum, Move, PieceTypeEnum, Position } from "../../types/types";
 import { Board } from "./Board";
 
 export class Game {
@@ -87,8 +87,34 @@ export class Game {
     }
 
     private update_game_status(): void {
-        // impl check, checkmate, stalemate detection
-        // do game analysis and return the checks
+        const current_player = this.game_state.currentPlayer;
+        const opponent_color: Color = current_player === 'white' ? 'black' : 'white';
+
+        // find the position of king for current_player
+        // check if king is under attack (check)
+        // check for checkmate and stalemate
+
+        const kingPosition = this.get_king_position(opponent_color);
+        if (!kingPosition) return;
+
+        const king_in_check = this.is_square_attacked(kingPosition, current_player);
+        const has_possible_moves = this.has_posiible_moves(opponent_color);
+
+        // king cannot move -> square being attacked and there are no possible moves ----------------> checkmate
+        // king can move -> square is not being attacked and there are no possible moves ------------> stalemate
+        // king can move -> square is being attacked but has possible moves ---------------------> check 
+        // else active
+
+        if (king_in_check && !has_possible_moves) {
+            this.game_state.gameStatus = GameStatusEnum.CHECKMATE;
+        } else if (!king_in_check && !has_possible_moves) {
+            this.game_state.gameStatus = GameStatusEnum.STALEMATE;
+        } else if (king_in_check) {
+            this.game_state.gameStatus = GameStatusEnum.CHECK;
+        } else {
+            this.game_state.gameStatus = GameStatusEnum.ACTIVE;
+        }
+
     }
 
     public get_game_state(): GameState {
@@ -110,4 +136,65 @@ export class Game {
         return piece.get_possible_moves(position, this.board);
     }
 
+    // check if square is attacekd
+    private is_square_attacked(position: Position, attackerColor: Color): boolean {
+        const board = this.board.get_board();
+
+        for (let y = 0; y < 8; y++) {
+            for (let x = 0; x < 8; x++) {
+                const piece = board[y]![x];
+
+                if (piece && piece.color === attackerColor) {
+                    const moves = piece.get_possible_moves({x, y}, this.board);
+                    if (moves.some(m => m.x === position.x && m.y === position.y)) {
+                        return true;
+                    };
+                };
+            }
+        }
+        return false;
+    }
+
+    private get_king_position(color: Color): Position | null {
+        const board = this.board.get_board();
+        for (let y = 0; y < 8; y++) {
+            for (let x = 0; x < 8; x++) {
+                const piece = board[y]![x];
+                if (piece && piece.type === PieceTypeEnum.KING && piece.color === color) {
+                    return { x, y }; 
+                }
+            }
+        }
+        return null;
+    }
+
+    // check if the  king is safe
+    private has_posiible_moves(color: Color): boolean {
+        const board = this.board.get_board();
+
+        for (let y = 0; y < 8; y++) {
+            for (let x = 0; x < 8; x++) {
+                const piece = board[y]![x];
+
+                if (piece && piece.color === color) {
+                    const moves = piece.get_possible_moves({x, y}, this.board);
+
+                    for (const move of moves) {
+                        const board_clone = this.board.clone_board();
+                        board_clone.make_move({x, y}, move);
+                        const king_position = this.get_king_position(color);
+
+                        if (king_position && !this.is_square_attacked(king_position, color === 'white' ? 'black' : "white")) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
+
+
